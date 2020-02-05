2ed9eb85c5b6ba8253727413258aef0eeeab4bae
greenfieldxd <monsterbig53@gmail.com>
2020-02-05 18:37:48 +0300

    minimum changes

--- Assets/TextQuestGame.cs
+++ Assets/TextQuestGame.cs
@@ -8,16 +8,24 @@ public class TextQuestGame : MonoBehaviour
     [Header("Elements")]
     public Text titleText;
     public Text contentText;
+    public Image titleBackground;
+    public Image contentBackground;
+    public Image mainBackground;
 
     [Header("Config")]
     public string text = "Game Title";
-    [TextArea(minLines:10, maxLines: 20)]public string content = "Перед вами два выхода: \n\n 1. Выход один \n 2. Выход два \n 3. Выйти";
+    public int titleTextSize = 30;
+    [TextArea(minLines:10, maxLines: 20)]
+    public string content = "Перед вами два выхода: \n\n 1. Выход один \n 2. Выход два \n 3. Выйти";
 
     // Start is called before the first frame update
     void Start()
     {
         titleText.text = text;
         contentText.text = content;
+        titleText.fontSize = titleTextSize;
+
+
         
     }
 
